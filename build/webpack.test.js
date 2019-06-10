0baca6ed4e0f3b969e1aebe71331a61999766deb
EvalGitHub <957010831@qq.com>
2019-06-11 00:01:38 +0800

    ui测试

---
+++ build/webpack.test.js
@@ -0,0 +1,61 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const webpackConfig = {
+    // webpack配置，针对测试脚本打包的compilation配置，与项目文件打包不相关
+    // 也可以引入独立的配置文件
+    mode: 'development',
+
+    resolve: {
+        extensions: ['.js', '.vue', '.json'],
+        modules: ['node_modules'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+          }
+    },
+    module: {
+        rules: [
+          {
+            test: /\.m?js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env'],
+              },
+            }
+          },
+          {
+            test: /\.vue$/,
+            loader: 'vue-loader',
+            options: {
+              compilerOptions: {
+                preserveWhitespace: false
+              }
+            }
+          },
+          {
+            test: /\.scss$/,
+            use: [
+              {
+                loader: "css-loader" // translates CSS into CommonJS
+              }, 
+              {
+                loader: "sass-loader" // compiles Sass to CSS
+              }
+            ]
+          }
+        //   {
+        //     test: /\.js$/,
+        //     include: path.resolve('src/lib'),
+        //     use: {
+        //         loader: 'istanbul-instrumenter-loader'
+        //     },
+        //  },
+        
+      ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+  }
+  
+module.exports = webpackConfig;
